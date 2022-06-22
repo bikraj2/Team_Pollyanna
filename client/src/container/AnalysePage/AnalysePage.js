6912802c77d2012742e96eadbec95b8f30f92e8c
dipenkhatri7 <khatridipen7@gmail.com>
2022-06-22 10:39:04 +0545

    route

---
+++ client/src/container/AnalysePage/AnalysePage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const AnalysePage = () => {
+  // const mapContainer = useRef(null);
+  // const map = useRef(null);
+  // const [lng, setLng] = useState(85.324);
+  // const [lat, setLat] = useState(27.7172);
+  // const [zoom, setZoom] = useState(13);
+  // const [boundData, setBoundData] = useState([]);
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (map.current) return; // initialize map only once
+  //   map.current = new maplibre.Map({
+  //     container: mapContainer.current,
+  //     style:
+  //       "https://api.baato.io/api/v1/styles/retro?key=bpk.RbCFuyprceXfPDgW7kxOENj-7iX968-ZRiYMv4nw9cwM",
+  //     center: [lng, lat],
+  //     zoom: zoom,
+  //   });
+  // });
+  return (
+    <div>
+      <h1>Second Page</h1>
+    </div>
+  );
+};
+
+export default AnalysePage;
