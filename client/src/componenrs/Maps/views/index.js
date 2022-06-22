d9625acf8994dea97675e3232d80efe518033934
erwinschrodinger1 <sunnynirjal@gmail.com>
2022-06-22 21:10:50 +0545

    khai k ho

--- client/src/componenrs/Maps/views/index.js
+++ client/src/componenrs/Maps/views/index.js
@@ -1,21 +1,8 @@
-import React, { useEffect } from "react";
-import maplibre from "maplibre-gl";
+import React from "react";
 import "maplibre-gl/dist/maplibre-gl.css";
 import useMap from "../hooks";
 export default function Map() {
-  const { mapContainer, map, lng, lat, zoom, setLat, setLng, setZoom } =
-    useMap();
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    console.log("Not current map so " + mapContainer.current);
-    map.current = new maplibre.Map({
-      container: mapContainer.current,
-      style:
-        "https://api.baato.io/api/v1/styles/retro?key=bpk.RbCFuyprceXfPDgW7kxOENj-7iX968-ZRiYMv4nw9cwM",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
+  const { mapContainer } = useMap();
   return (
     <div
       style={{
